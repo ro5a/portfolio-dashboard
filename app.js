014e483da28767a215940f1dc4edf47effdc957e
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-02-22 15:12:01 +0300

    create dasboard

---
+++ app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+var port = process.env.PORT || 3000;
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+app.get("/dasboard", function(req, res){
+
+  res.render("dashboard"); 
+});
+app.get("/experience", function(req, res){
+
+  res.render("experience"); 
+});
+app.get("/skills", function(req, res){
+
+  res.render("skills"); 
+});
+app.listen(port, function(){
+  console.log("server is running on port" + port);
+});
+module.exports = app;
