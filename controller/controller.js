54018713da51567957d2b23400a7f1ead96c902e
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-02-23 09:52:51 +0300

    create new experince

--- controller/controller.js
+++ controller/controller.js
@@ -1,7 +1,22 @@
 var UserDB=require('../model/model');
 //new experience
 exports.create=(req,res)=>{
-
+if(!req.body){
+     res.status(400).send({message:"content can not be empty!"});
+     return;
+}
+const exper=new UserDB({
+    name:req.body.name
+})
+exper.save(exper)
+.then(data=>{
+     res.send(data)
+})
+.catch(err=>{
+    res.status(500).send({
+        message:err.message || "some error acurred white created opertion"
+    });
+});
 }
 //rettrive and return
 exports.find=(req,res)=>{
@@ -13,5 +28,5 @@ exports.update=(req,res)=>{
 }
 //delete
 exports.delete=(req,res)=>{
-    
+
 }
