75a1f1765951499157f1c5acb1958fbfde60050d
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-03-07 11:45:25 +0300

    update add file

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var flash = require('express-flash');
+var session = require('express-session');
+var usersRouter = require('./routes/index');
+var mongoose = require('mongoose')
+var app = express();
+// view engine setup
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// Connected DB
+mongoose.connect('mongodb://localhost:27017/dashboard', (err) => {
+    if (err) console.log(err);
+    else console.log("DB Conected...");
+});
+app.use(session({
+secret: '123456catr',
+resave: false,
+saveUninitialized: true,
+cookie: {
+maxAge: 60000
+}
+}))
+app.use(flash());
+app.use('/', usersRouter);
+app.get('/test', function(req, res, next) {
+
+     res.write("jh");
+     res.end();
+})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+next(createError(404));
+});
+// error handler
+app.use(function(err, req, res, next) {
+// set locals, only providing error in development
+res.locals.message = err.message;
+res.locals.error = req.app.get('env') === 'development' ? err : {};
+// render the error page
+res.status(err.status || 500);
+res.render('error');
+});
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,113 +3,120 @@ var router = express.Router();
 
 var Emodel = require('../models/Emodel');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-Emodel.find((err, docs) => {
-if (!err) {
-res.render("index", {
-data: docs
-});
-} else {
-console.log('Failed to retrieve the Users List: ' + err);
-}
-});
+router.get('/', function (req, res, next) {
+    
+    Emodel.find((err, docs) => {
+        if (!err) {
+            res.render("index", {
+                data: docs 
+            });
+        } else {
+            console.log('Failed to retrieve the Users List: ' + err);
+        }
+    });
 });
 // SHOW ADD USER FORM
-router.get('/add', function(req, res, next) {
+router.get('/add', function (req, res, next) {
 
-res.render('/add', {
-title: 'Add New experience',
-name: ''
-})
+    res.render('add', {
+        title: 'Add New experience',
+        name: ''
+    })
 })
 // ADD NEW USER POST ACTION
-router.post('/add', function(req, res, next) {
-req.assert('name', 'Name is required').notEmpty() //Validate name
-var errors = req.validationErrors()
-if (!errors) { //No errors were found.  Passed Validation!
-var userDetails = new Emodel({
-name: req.body.name
-});
-userDetails.save((err, doc) => {
-if (!err){req.flash('success', 'User added successfully!');
-res.redirect('/index');}
+router.post('/add', function (req, res, next) {
 
-else
-res.render('/add', {
-title: 'Add New User',
-name: user.name
-})
-});
-} else { //Display errors to user
-var error_msg = ''
-errors.forEach(function(error) {
-error_msg += error.msg + '<br>'
-})
-req.flash('error', error_msg)
-res.render('/add', {
-title: 'Add New User',
-name: req.body.name
-})
-}
+    // req.assert('name', 'Name is required').notEmpty() //Validate name
+    console.log(req.body)
+    //   var errors = req.validationErrors()
+    // if (!errors) { //No errors were found.  Passed Validation!
+        var userDetails = new Emodel({
+            name: req.body.name
+        });
+        userDetails.save((err, doc) => {
+            if (!err) {
+                req.flash('success', 'User added successfully!');
+                res.redirect('/');
+            }
+    
+            else
+                res.render('add', {
+                    title: 'Add New User',
+                    name: user.name
+                })
+        });
+     
+    //  else { //Display errors to user
+    //     var error_msg = ''
+    //     errors.forEach(function (error) {
+    //         error_msg += error.msg + '<br>'
+    //     })
+    //     req.flash('error', error_msg)
+    //     res.render('add', {
+    //         title: 'Add New User',
+    //         name: req.body.name
+    //     })
+    // }
+   
 })
 // SHOW EDIT USER FORM
-router.get('/edit/(:id)', function(req, res, next) {
-userModel.findById(req.params.id, (err, doc) => {
-if (!err) {
-res.render("/edit", {
-title: "Update experience Details",
-data: doc
-});
-} else {
-req.flash('error', 'experience not found with id = ' + req.params.id)
-res.redirect('/index')
-}
-});
+router.get('/edit/(:id)', function (req, res, next) {
+    Emodel.findById(req.params.id, (err, doc) => {
+        if (!err) {
+            res.render("edit", {
+                title: "Update experience Details",
+                data: doc
+            });
+        } else {
+            req.flash('error', 'experience not found with id = ' + req.params.id)
+            res.redirect('/index')
+        }
+    });
 })
 // EDIT USER POST ACTION
-router.post('/update/:id', function(req, res, next) {
-req.assert('name', 'Name is required').notEmpty() //Validate nam
-var errors = req.validationErrors()
-if (!errors) {
-var experience = {
-name: req.sanitize('name').escape().trim(),
-}
-userModel.findByIdAndUpdate(req.body.id, {
-name: req.body.name
-} , function(err, data) {
-if (err) {
-req.flash('error', 'Something Goes to Wrong!');
-res.render('index');
-} else {
-req.flash('success', 'User has been updated successfully!');
-res.redirect('/index');
-}
-});
-} else { //Display errors to user
-var error_msg = ''
-errors.forEach(function(error) {
-error_msg += error.msg + '<br>'
-})
-req.flash('error', error_msg)
-/**
-* Using req.body.name 
-* because req.param('name') is deprecated
-*/
-res.render('/edit', {
-title: 'Edit experience',
-id: req.params.id,
-name: req.body.name
-})
-}
+router.post('/update/:id', function (req, res, next) {
+    // req.assert('name', 'Name is required').notEmpty() //Validate nam
+    // var errors = req.validationErrors()
+    // if (!errors) {
+        var experience = {
+            name: req.sanitize('name').escape().trim(),
+        }
+        Emodel.findByIdAndUpdate(req.body.id, {
+            name: req.body.name
+        }, function (err, data) {
+            if (err) {
+                req.flash('error', 'Something Goes to Wrong!');
+                res.render('index');
+            } else {
+                req.flash('success', 'User has been updated successfully!');
+                res.redirect('/index');
+            }
+        });
+    // } else { //Display errors to user
+    //     var error_msg = ''
+    //     errors.forEach(function (error) {
+    //         error_msg += error.msg + '<br>'
+    //     })
+        req.flash('error', error_msg)
+        /**
+        * Using req.body.name 
+        * because req.param('name') is deprecated
+        */
+        res.render('/edit', {
+            title: 'Edit experience',
+            id: req.params.id,
+            name: req.body.name
+        })
+    
 })
 // DELETE USER
-router.get('/delete/(:id)', function(req, res, next) {
-userModel.findByIdAndRemove(req.params.id, (err, doc) => {
-if (!err) {
-res.redirect('/index');
-} else {
-console.log('Failed to Delete user Details: ' + err);
-}
-});
+router.get('/delete/(:id)', function (req, res, next) {
+    Emodel.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) {
+            res.redirect('/index');
+        } else {
+            console.log('Failed to Delete user Details: ' + err);
+        }
+    });
 })
 module.exports = router;
